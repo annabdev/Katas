a235a5d56fdf2d8eb3ba1d1d22b5aafa97e548f3
Anna Beagley <annabeagley@Annas-MacBook-Air.local>
2019-04-11 13:39:28 -0600

    added 2nd exercise

---
+++ string-template2/index.js
@@ -0,0 +1,43 @@
+// 2: template strings - multiline
+// To do: make all tests pass, leave the asserts unchanged!
+// Follow the hints of the failure messages!
+
+describe('Template string, can contain multiline content', function() {
+    it('wrap it in backticks (`) and add a newline, to span across two lines', function() {
+      // var normalString = `line1 //// line3`;
+      var normalString = `line1\n\nline3`;
+      
+      // wrote in \n which creates a new line, did this twice to get from line 1 to line 3
+  
+      assert.equal(normalString, 'line1\n\nline3');
+    });
+    it('even over more than two lines', function() {
+      // var multiline = ``;
+      var multiline = `\n\n\n`;
+      
+      //same as before just added one "\n"
+          
+        
+      assert.equal(multiline.split('\n').length, 4);
+    });
+    describe('and expressions inside work too', function() {
+      var x = 42;
+      it('like simple variables', function() {
+        // var multiline = `line 1 $ {x}`;
+        var multiline = `line 1\n\n      ${x}`;
+        
+        //just had to add a bunch of extra spaces
+  
+        assert.equal(multiline, 'line 1\n\n      42');
+      });
+      it('also here spaces matter', function() {
+        // var multiline = ``;
+        var multiline = `\n\n${x}`;
+        
+        //added two new lines and a function calling x
+  
+        assert.equal(multiline, '\n\n42');
+      });
+    });
+  });
+  
